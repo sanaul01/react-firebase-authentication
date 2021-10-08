52033296b0f77f8e406c50f7c10ee02b8d0cdb24
sanaul01 <mdsanaulislam154@gmail.com>
2021-10-09 01:36:22 +0600

    click handelar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-import logo from './logo.svg';
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.init';
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () =>{
+    signInWithPopup(auth, googleProvider)
+    .then(result =>{
+      const user = result.user
+      console.log(user)
+    })
+  }
+
+  const handleEmailChenge = e =>{
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChenge = e =>{
+    setPassword(e.target.value)
+  }
+
+  const handleRegistration = e =>{
+    console.log(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result =>{
+      const user = result.user
+      console.log(user)
+    })
+    e.preventDefault();
+  }
   return (
-    <div className="App">
-      <h2>React Firebase Authentication</h2>
+    <div className="mx-5">
+      <h2 className="text-primary">Please registration</h2>
+      <form onSubmit={handleRegistration}>
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmailChenge} type="email" className="form-control" id="inputEmail3" required/>
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input onBlur={handlePasswordChenge} type="password" className="form-control" id="inputPassword3" required/>
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck1"/>
+              <label className="form-check-label" htmlFor="gridCheck1">
+                Example checkbox
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">register</button>
+      </form>
+      <br /><br /><br />
+      <div>-----------------------------</div>
+      <br /><br /><br />
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCcWKH3fcxIm-8YkU7gqjGDIBg0qPuleM8",
+    authDomain: "react-firebase-authentic-d6f7d.firebaseapp.com",
+    projectId: "react-firebase-authentic-d6f7d",
+    storageBucket: "react-firebase-authentic-d6f7d.appspot.com",
+    messagingSenderId: "1018373762295",
+    appId: "1:1018373762295:web:be0e9c14e3e86ae4c1f7f2"
+  };
+
+  export default firebaseConfig;
