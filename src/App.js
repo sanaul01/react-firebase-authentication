810df5a055e0b02868df1f3bc046cd9c50bb7c52
sanaul01 <mdsanaulislam154@gmail.com>
2021-10-09 14:52:18 +0600

    login process added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.init';
@@ -9,6 +9,8 @@ const googleProvider = new GoogleAuthProvider();
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [isLogin, setIsLogin] = useState(false);
   const auth = getAuth();
 
   const handleGoogleSignIn = () =>{
@@ -17,6 +19,10 @@ function App() {
       const user = result.user
       console.log(user)
     })
+  }
+
+  const toggleLogIn = e =>{
+    setIsLogin(e.target.checked)
   }
 
   const handleEmailChenge = e =>{
@@ -28,17 +34,56 @@ function App() {
   }
 
   const handleRegistration = e =>{
+    e.preventDefault();
     console.log(email, password)
+    if(password.length < 6){
+      setError('please enter must be 6 caracter')
+      return;
+    }
+    if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+      setError('You should be must use 2 uppercase')
+      return;
+    }
+    
+    // isLogin? processLogin(email, password) : registerNewUser(email, password)
+
+    // or
+
+    if(isLogin){
+      processLogin(email, password)
+    }
+    else{
+      registerNewUser(email, password)
+    }
+    
+  }
+
+  const processLogin = (email, password) =>{
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result =>{
+      const user = result.user
+      console.log(user)
+      setError('');
+    })
+    .catch(error =>{
+      setError(error.message);
+    })
+  }
+
+  const registerNewUser = (email, password) =>{
     createUserWithEmailAndPassword(auth, email, password)
     .then(result =>{
       const user = result.user
       console.log(user)
+      setError('');
     })
-    e.preventDefault();
+    .catch(error =>{
+      setError(error.message);
+    })
   }
   return (
     <div className="mx-5">
-      <h2 className="text-primary">Please registration</h2>
+      <h2 className="text-primary">Please { isLogin ? 'Login': 'register'}</h2>
       <form onSubmit={handleRegistration}>
         <div className="row mb-3">
           <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
@@ -55,14 +100,17 @@ function App() {
         <div className="row mb-3">
           <div className="col-sm-10 offset-sm-2">
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" id="gridCheck1"/>
+              <input onChange={toggleLogIn} className="form-check-input" type="checkbox" id="gridCheck1"/>
               <label className="form-check-label" htmlFor="gridCheck1">
-                Example checkbox
+                Allready Registered?
               </label>
             </div>
           </div>
+          <div className="row-mb-3 text-danger">{error}</div>
         </div>
-        <button type="submit" className="btn btn-primary">register</button>
+        <button type="submit" className="btn btn-primary">
+          { isLogin ? 'Login': 'register'}
+          </button>
       </form>
       <br /><br /><br />
       <div>-----------------------------</div>
